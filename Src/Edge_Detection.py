dd0b1e7a0c5c291c3434bc7ccf4c9ce4bc821a10
KidChaos <genesis.enedeh@gmail.com>
2021-12-30 13:18:04 +0100

    put in a new property and added in a new method

--- Src/Edge_Detection.py
+++ Src/Edge_Detection.py
@@ -7,6 +7,7 @@ class EdgeDetection:
     def __init__(self, image_path: str, line_color: tuple):
         if os.path.isfile(image_path):
             self.image_path = image_path
+            self.image = cv2.imread(image_path, cv2.IMREAD_COLOR)
         else:
             print("This Image Path Dose Not Exist")
         if len(line_color) == 3:
@@ -14,5 +15,5 @@ class EdgeDetection:
         else:
             print("This Color Code Is Incorrect")
 
-
-
+    def resize(self, width=500, height=200):
+        self.image = imutils.resize(self.image, width=width, height=height)
